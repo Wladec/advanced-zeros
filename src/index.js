59581e8a7f23816b743d8ceefa23b71443853dd9
Wladec <vladick.tsemenko@gmail.com>
2019-03-22 22:54:23 +0300

    add getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let arr = [];
+    let zerosCount = 0;
+    i = 2;
+    let j = 0;
+    let factor = 0;
+    let countFactor = 0;
+    if ( base > 2) {
+    do {
+        if (base % i == 0){
+            a = base / i;
+            arr.push(i);
+            base = a;
+        }
+        else i++;
+    }
+    while (i < base);
+     arr.push(i);
+    
+    factor = Math.max.apply(null, arr);
+
+    for (j; j < arr.length; j++){
+        if (arr[j] == factor) countFactor++;
+        }
+    }
+    else {
+    factor = base;
+    countFactor = 1;
+    }
+
+    for (let i = factor; number / i >= 1; i *= factor){
+        zerosCount += Math.floor(number / i);
+    };
+    return Math.floor(zerosCount / countFactor);
 }
